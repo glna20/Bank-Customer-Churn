b60c9c7ff0c8bcf6981d043e5872154a52332392
OGE <106489347+glna20@users.noreply.github.com>
2025-06-25 21:53:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import joblib
 import pandas as pd 
 import numpy as np
 
-!pip install xgboost
 #!echo "streamlit\njoblib\npandas\nnumpy\nscikit-learn" > requirements.txt
 
 ct = joblib.load("ct_xgb.pkl")
